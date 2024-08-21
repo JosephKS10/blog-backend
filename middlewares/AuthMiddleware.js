d25a9b287624f527c4dd3a3bbaa048f2335cdc9f
JosephKS10 <jjbrothersdelhi@hotmail.com>
2024-08-21 16:06:59 +0530

    Some changes in the index.js file

--- middlewares/AuthMiddleware.js
+++ middlewares/AuthMiddleware.js
@@ -1,4 +1,4 @@
-const authMiddleware = (req, res, next) => {
+const AuthMiddleware = (req, res, next) => {
   const jwt = require('jsonwebtoken');
 
   const token = req.headers.authorization?.split(' ')[1]; 
@@ -13,4 +13,4 @@ const authMiddleware = (req, res, next) => {
   }
 };
 
-module.exports = authMiddleware;
+module.exports = AuthMiddleware;
